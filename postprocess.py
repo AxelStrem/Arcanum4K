1abca529fd703fc902689d058497a6e362fdd1f7
Alexey Stremov <axelstrem@gmail.com>
2016-12-20 20:07:02 +0300

    Script for alpha-merging and 0.5x scaling

---
+++ postprocess.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+from PIL import Image
+import numpy as np
+
+def quantizetopalette(silf, palette, dither=False):
+    """Convert an RGB or L mode image to use a given P image's palette."""
+
+    silf.load()
+
+    # use palette from reference image
+    palette.load()
+    if palette.mode != "P":
+        raise ValueError("bad mode for palette image")
+    if silf.mode != "RGB" and silf.mode != "L":
+        raise ValueError(
+            "only RGB or L mode images can be quantized to a palette"
+            )
+    im = silf.im.convert("P", 1 if dither else 0, palette.im)
+    # the 0 above means turn OFF dithering
+    return silf._makeself(im)
+
+
+for root, dirs, files in os.walk('bmps_out'):
+    for name in dirs:
+        dirname = str('x30_out/')+os.path.join(root[8:],name)
+        dirname2 = str('x15_out/')+os.path.join(root[8:],name)
+        if not os.path.isdir(dirname):
+            os.mkdir(dirname)
+        if not os.path.isdir(dirname2):
+            os.mkdir(dirname2)
+    for name in files:
+        if name[-3:].upper()=='BMP':
+            print (str("applying alpha: ")+os.path.join(root[8:],name[:-4]))
+            dstname = str('x30_out/')+os.path.join(root[8:],name)
+            dstname2 = str('x15_out/')+os.path.join(root[8:],name)
+            brdname = str('border_out/')+os.path.join(root[8:],name)
+            origname = str('bmps_in/')+os.path.join(root[8:],name)
+            srcname = os.path.join(root,name)
+            #subprocess.call(["ArtConverter.exe",srcname,dstname])
+            img = Image.open(srcname)
+            smallsize = ((img.size[0]+1)//2, (img.size[1]+1)//2)
+            img2 = img.resize(smallsize, Image.ANTIALIAS)
+            orig = Image.open(origname)
+            palette = orig.getpalette()
+            if palette!=None:
+                alpha  = palette[0:3]
+                border = Image.open(brdname)
+                img = quantizetopalette(img, orig, dither=False)
+                pix = np.array(img,dtype='uint8')
+                apix = np.array(border,dtype='uint8')
+                pix.flags.writeable=True
+                pix[apix==0]=0
+                img = Image.fromarray( np.asarray( np.clip(pix,0,255), dtype="uint8"), "P" )
+                
+                img2 = quantizetopalette(img2, orig, dither=False)
+                pix = np.array(img2,dtype='uint8')
+                border = border.resize(smallsize, Image.NEAREST)
+                apix = np.array(border,dtype='uint8')
+                pix.flags.writeable=True
+                pix[apix==0]=0
+                img2 = Image.fromarray( np.asarray( np.clip(pix,0,255), dtype="uint8"), "P" )
+                
+                img.putpalette(palette)
+                img.save(dstname)
+
+                img2.putpalette(palette)
+                img2.save(dstname2)
+            else:
+                img.save(dstname)
+                img2.save(dstname2)
